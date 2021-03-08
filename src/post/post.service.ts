616fde161a8d4fce3468d9863f3e4936826284cc
mil <1363944642@qq.com>
2021-03-08 17:31:58 +0800

    在单个内容中标记当前用户是否赞过内容

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -211,7 +211,16 @@ export const getPostsTotalCount = async (options: GetPostsOptions) => {
 /**
  * 按 ID 调取内容
  */
-export const getPostById = async (postId: number) => {
+export interface GetpostByIdOptions {
+  currentUser?: TokenPayload;
+}
+export const getPostById = async (
+  postId: number,
+  options: GetPostsOptions = {}
+) => {
+  const {
+    currentUser: { id: userId },
+  } = options;
   // 准备查询
   const statement = `
     SELECT
@@ -222,7 +231,14 @@ export const getPostById = async (postId: number) => {
       ${sqlFragment.totalComments},
       ${sqlFragment.file},
       ${sqlFragment.tags},
-      ${sqlFragment.totalLikes}
+      ${sqlFragment.totalLikes},
+      (
+        SELECT COUNT(user_like_post.postId)
+        FROM user_like_post
+        WHERE
+          user_like_post.postId = post.id
+          && user_like_post.userId = ${userId}
+      ) AS liked
     FROM post
     ${sqlFragment.leftJoinUser}
     ${sqlFragment.leftJoinOneFile}
